8feb3c522674275d78cde5cdd571c776f43ad7ec
EngrKosi <94290491+EngrKosi@users.noreply.github.com>
2022-11-21 13:59:03 +0000

    Create First_python.py
    Python code to try out new skills

---
+++ First_python.py
@@ -0,0 +1,10 @@
+# trying out my python skills 
+ab = 12;
+ac = 14;
+ad = 10;
+result = (ab*ac)-ad;
+print(result)
+if result > 10:
+  print("The student has passed")
+else:
+  print("The student failed")
